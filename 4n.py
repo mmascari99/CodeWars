98175d08e2eacf6d2b4848330eaef03087c38a32
Michael Mascari <mascarimichael71@gmail.com>
2020-05-21 12:20:34 -0500

    Add files via upload

---
+++ 4n.py
@@ -0,0 +1,9 @@
+cases = int(input())
+while cases > 0:
+    sum = 0
+    n = int(input())
+    while n > 1:
+        sum = sum + ((4 * n - 4) * ((n - 1) / 2))
+        n = n - 2
+    print(int(sum))
+    cases = cases - 1
